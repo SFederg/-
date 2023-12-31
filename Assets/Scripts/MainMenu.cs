59e7ae54641c721449e4fa84e5ca924e3d2cf7d6
Sergey Fedukov <sfedyukov16@gmail.com>
2023-12-31 14:49:17 +0700

    Добавлены обработчики события при нажатии на кнопки в главном меню; Добавлено подменю 'Настройки'; Добавлен выход из игры и переход к игре при нажатии на соответствующие кнопки

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private Button previousButton;
+    [SerializeField] private TextMeshProUGUI levelDifficultlyText;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(delegate ()
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        settingsButton.onClick.AddListener(delegate ()
+        {
+            playButton.gameObject.SetActive(false);
+            settingsButton.gameObject.SetActive(false);
+            quitButton.gameObject.SetActive(false);
+            dropdown.gameObject.SetActive(true);
+            previousButton.gameObject.SetActive(true);
+            levelDifficultlyText.gameObject.SetActive(true);
+
+            previousButton.onClick.AddListener(delegate ()
+            {
+                playButton.gameObject.SetActive(true);
+                settingsButton.gameObject.SetActive(true);
+                quitButton.gameObject.SetActive(true);
+                dropdown.gameObject.SetActive(false);
+                previousButton.gameObject.SetActive(false);
+                levelDifficultlyText.gameObject.SetActive(false);
+            });
+
+        });
+
+        quitButton.onClick.AddListener(delegate ()
+        {
+            Application.Quit();
+        });
+    }
+}
